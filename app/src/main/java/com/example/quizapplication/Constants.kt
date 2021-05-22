ed8daef2db91b6f0ae96b0bf445365a33b110aef
Ankit-akumar <as2262011@gmail.com>
2021-05-23 00:34:00 +0530

    Removed files

--- app/src/main/java/com/example/quizapplication/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quizapplication
-
-object Constants {
-    var CURRENT_USERID = 0L
-}
--- app/src/main/java/com/example/quizapplication/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quizapplication
-
-class User(
-    val userid: Long = 0L,
-    var username: String = "username",
-    var email: String = "email",
-    var password: String = "password",
-    var stats: Long = 0L
-)
